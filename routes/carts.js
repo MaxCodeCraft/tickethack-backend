3a60b9b6fe4f6d35adde61a8bde87d92ee5aa588
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-16 17:06:56 +0200

    update

--- routes/carts.js
+++ routes/carts.js
@@ -7,4 +7,17 @@ router.get("/", (req, res) => {
   Cart.find().then((cartData) => res.json({ cartData }));
 });
 
+router.post("/add", (req, res) => {
+  const newTrip = new Cart({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    hour: req.body.hour,
+    price: req.body.price,
+  });
+  newTrip.save().then((data) => {
+    res.json({ result: true });
+    console.log(data);
+  });
+});
+
 module.exports = router;
